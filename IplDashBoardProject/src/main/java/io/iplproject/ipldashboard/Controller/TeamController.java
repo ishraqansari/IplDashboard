3414eb1f6888918014583bc5ad79e062f25e362c
Ishraq Ansari <ishraq.ansari@tothenew.com>
2021-05-28 09:29:13 +0530

    HomePage added and styled.

--- IplDashBoardProject/src/main/java/io/iplproject/ipldashboard/Controller/TeamController.java
+++ IplDashBoardProject/src/main/java/io/iplproject/ipldashboard/Controller/TeamController.java
@@ -25,19 +25,17 @@ public class TeamController {
         this.matchRepository = matchRepository;
     }
 
-
-    @GetMapping("/team")
-    public Iterable<Team> getAllTeam() {
-        return this.teamRepository.findAll();
-    }
-
     @GetMapping("/team/{teamName}")
     public Team getTeam(@PathVariable String teamName) {
         Team team = this.teamRepository.findByTeamName(teamName);
         team.setMatches(matchRepository.findLatestMatchesByTeam(teamName,4));
-
         return team;
     }
+    @GetMapping("/team")
+    public Iterable<Team> etAllTeam() {
+        return this.teamRepository.findAll();
+    }
+
     @GetMapping("/team/{teamName}/matches")
     public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year) {
         LocalDate startDate = LocalDate.of(year, 1, 1);
